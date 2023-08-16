833a2dd21865e482749be6d06939cc6f7bf317fe
pcsuke <110383951+sukepc0824@users.noreply.github.com>
2023-08-16 14:24:33 +0900

    Update engine-data.js

--- js/engine-data.js
+++ js/engine-data.js
@@ -26,5 +26,33 @@ const engine = {
         suggestAPI: undefined,
         searchLink: 'https://twitter.com/search?q=',
         startPage: 'https://twitter.com/home'
+    },
+    amazon: {
+        name:"Amazon",
+        icon:'img/amazon.svg',
+        suggestAPI: undefined,
+        searchLink:'https://www.amazon.co.jp/s?k=',
+        startPage:'https://amazon.co.jp'
+    },
+    yahoo: {
+        name:"Yahoo!-JP",
+        icon:'img/yahoo.svg',
+        suggestAPI:'https://ff.search.yahoo.com/gossip?output=fxjson&command=',
+        searchLink:'https://search.yahoo.co.jp/search?p=',
+        startPage:'https://yahoo.co.jp'
+    },
+    facebook: {
+        name:"facebook",
+        icon:'img/facebook.svg',
+        suggestAPI:undefined,
+        searchLink:'https://facebook.com/search/?q=',
+        startPage:'https://facebook.com'
+    },
+    bing: {
+        name:"bing",
+        icon:'img/bing.svg',
+        suggestAPI:undefined,
+        searchLink:'https://www.nicovideo.jp/search/',
+        startPage:'https://nicovideo.jp'
     }
 }
